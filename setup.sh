cba9861b2d7bbc9105a275870d72fbb7559dbe50
Brian Malehorn <brian.malehorn@opendoor.com>
2022-02-23 16:29:53 -0800

    use explicit dotfiles dir I found

--- setup.sh
+++ setup.sh
@@ -2,12 +2,16 @@
 
 set -xeuo pipefail
 
-# change to script directory
-cd "$(dirname "$0")"
+dotfiles_dir=/workspaces/.codespaces/.persistedshare/dotfiles
+
+if ! [ -d $dotfiles_dir ]; then
+  echo "Oh no! $dotfiles_dir not found"
+  exit 1
+fi
 
 # shellcheck disable=SC2129
 echo hello >> ~/setup.txt
 
-cp -rv ./* ~/ 1>&2 >> ~/setup.txt
+cp -rv "$dotfiles_dir"/* ~/ 1>&2 >> ~/setup.txt
 
 echo "setup.sh" >> ~/setup.txt
